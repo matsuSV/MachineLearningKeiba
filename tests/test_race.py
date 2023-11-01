cb5d1328a7c75077d23178c1100d5332f0bdaafe
shota matsumoto <conpro2005@gmail.com>
2023-11-02 08:32:18 +0900

    単体試験追加 def test__scrape():

--- tests/test_race.py
+++ tests/test_race.py
@@ -1,5 +1,7 @@
 # from unittest.mock import patch
 import apps.race as apps_race
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
 
 
 def test_init():
@@ -7,25 +9,25 @@ def test_init():
     期待動作：
     　 Raceクラスを初期化したインスタンス内のメンバ変数が以下の状態となっていること
     　　　race_id_list：コンストラクタへ渡された引数の値
-    　　　downloaded_ids：既に取得しているレースID
-    　　　pd_all_races：空のデータフレーム
     """
-    # read()関数をモックする
+    # read()関数をモックする(リファクタリングして使わなくなった、けど参考として残している)
     # with patch('apps.file.read', return_value=['20190101010101', '20190101010102', '20190101010103']):
     race = apps_race.Race(['201901010101'])
     assert race.race_id_list == ['201901010101']
-    # assert race.downloaded_ids == ['20190101010101', '20190101010102', '20190101010103']
 
 
 def test__scrape():
     """
-
-    :return:
+    期待動作：
+    　 １．存在するレースIDの場合、レース情報がDataFrameで取得できること
+       2. 存在しないレースIDの場合、空のDataFrameが取得できること
     """
-    # exists race page
-    df = apps_race.Race._scrape('201901010101')
-    print(df)
+    # 1. exists race page
+    actual_df = apps_race.Race._scrape('201901010101')
+    expected_df = pd.read_csv('csv/test__scrape.csv')
+    assert_frame_equal(actual_df, expected_df)
 
-
-
-    # nothing
+    # 2. nothing
+    actual_df = apps_race.Race._scrape('101901010101')
+    expected_df = pd.DataFrame()
+    assert_frame_equal(actual_df, expected_df)
